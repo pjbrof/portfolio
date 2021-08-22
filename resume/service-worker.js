0d62b5f472632aef82b3831e65ec2831251c9b59
Patrick Brophy <pjbrof@gmail.com>
2021-08-21 22:09:27 -0400

    pwa and 100 lighthouse score

---
+++ resume/service-worker.js
@@ -0,0 +1,9 @@
+(function () {
+  self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+})();
