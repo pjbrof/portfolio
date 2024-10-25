e509a8e93c974d3476c8c27ca8a72ab504bf5b8d
pjbrof <pjbrof@gmail.com>
2024-10-24 23:39:22 -0400

    docker file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.send({ message: 'hello world' });
+});
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
